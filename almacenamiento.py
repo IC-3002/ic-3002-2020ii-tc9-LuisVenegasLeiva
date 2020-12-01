664928f88f922c35e2f0f5e0663f773af0e59f1a
LuisVenegas <@luvenegas>
2020-11-30 20:13:51 -0600

    se agrega la funcion de maximizar

--- almacenamiento.py
+++ almacenamiento.py
@@ -1,2 +1,19 @@
 def maximizar(As, D):
-    raise NotImplementedError()
+	ordenados=[]
+	tam=0
+	As.sort(key=lambda x: x[1])
+	for i in As:
+		if tam+i[1] < D:
+			ordenados.append(i)
+			tam=tam+i[1]
+	return ordenados
+
+
+"""
+
+As = [('archivo1', 10), ('archivo2', 5), ('archivo3', 3),
+              ('archivo4', 8), ('archivo5', 9), ('archivo6', 1)]
+D=10
+
+maximizar(As,D)
+"""

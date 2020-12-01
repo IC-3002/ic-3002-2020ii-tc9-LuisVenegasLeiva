d681965a364d8d1f921f966839f65e468d3750f5
LuisVenegas <@luvenegas>
2020-11-30 20:18:57 -0600

    se modifica la funcion para logarar tiempo logaritmico

--- almacenamiento.py
+++ almacenamiento.py
@@ -2,18 +2,8 @@ def maximizar(As, D):
 	ordenados=[]
 	tam=0
 	As.sort(key=lambda x: x[1])
-	for i in As:
-		if tam+i[1] < D:
-			ordenados.append(i)
-			tam=tam+i[1]
+	for i in range (len(As)):
+		if tam+As[i][1] < D:
+			ordenados.append(As[i])
+			tam=tam+As[i][1]
 	return ordenados
-
-
-"""
-
-As = [('archivo1', 10), ('archivo2', 5), ('archivo3', 3),
-              ('archivo4', 8), ('archivo5', 9), ('archivo6', 1)]
-D=10
-
-maximizar(As,D)
-"""
